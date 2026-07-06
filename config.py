a3199708247ece0a06b8b21f22d7cabb06ba3856
Redbox777 <thenewtonflow@gmail.com>
2026-07-06 16:35:52 +0300

    Change config to use hardcoded values instead of env vars
    Replaced environment variable retrieval with hardcoded values.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-TOKEN      = os.environ.get("8707826926:AAFP1ZEJIPRW-HGgpEQ7HMgIRLRv20c9X_4")
-DB_FILE    = os.environ.get("DB_FILE", "gatherly.db")
-GROQ_KEY   = os.environ.get("GROQ_KEY", "")
-YANDEX_KEY = os.environ.get("YANDEX_KEY", "")
+TOKEN      = ("8707826926:AAFP1ZEJIPRW-HGgpEQ7HMgIRLRv20c9X_4")
+DB_FILE    = ("DB_FILE", "gatherly.db")
+GROQ_KEY   = ("GROQ_KEY", "")
+YANDEX_KEY = ("YANDEX_KEY", "")
