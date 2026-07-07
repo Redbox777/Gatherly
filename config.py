f27d3e78580f57714f4cd0652d17ffc951010ef0
Redbox777 <redbox777@example.com>
2026-07-07 08:47:01 +0300

    Добавлены .env, .gitignore, README, обновлён config

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
 import os
 
-TOKEN      = os.environ.get("TOKEN", "8707826926:AAFIkE7qyK-2uehbd1FDVvIAi_9bH_TcAnw")
+try:
+    from dotenv import load_dotenv
+    load_dotenv()
+except ImportError:
+    pass
+
+TOKEN      = os.environ.get("TOKEN", "ВСТАВЬ_ТОКЕН")
 DB_FILE    = os.environ.get("DB_FILE", "gatherly.db")
 GROQ_KEY   = os.environ.get("GROQ_KEY", "")
 YANDEX_KEY = os.environ.get("YANDEX_KEY", "")
