0b20aa802e89199f9eb5c9161aaff1d1227131b4
Redbox777 <thenewtonflow@gmail.com>
2026-07-07 08:49:50 +0300

    Update TOKEN with actual value
    Replaced placeholder token with actual token value.

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     pass
 
-TOKEN      = os.environ.get("TOKEN", "ВСТАВЬ_ТОКЕН")
+TOKEN      = os.environ.get("TOKEN", "8707826926:AAELSd4Ad7uMA8_YUtZuM5dof6OnTRqzB2Y")
 DB_FILE    = os.environ.get("DB_FILE", "gatherly.db")
 GROQ_KEY   = os.environ.get("GROQ_KEY", "")
 YANDEX_KEY = os.environ.get("YANDEX_KEY", "")
